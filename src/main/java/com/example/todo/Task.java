a65d033aa31a023cdbfd6aed552938e439284b79
katekiselevich <kiselkath@gmail.com>
2025-04-11 14:40:05 +0200

    Added priority field to Task class

--- src/main/java/com/example/todo/Task.java
+++ src/main/java/com/example/todo/Task.java
@@ -3,10 +3,12 @@ package com.example.todo;
 public class Task {
     private String title;
     private boolean isDone;
+    private String priority;
 
     public Task(String title) {
         this.title = title;
         this.isDone = false;
+        this.priority = priority;
     }
 
     public String getTitle() {
@@ -15,6 +17,10 @@ public class Task {
 
     public boolean isDone() {
         return isDone;
+    }
+
+    public String getPriority() {
+        return priority;
     }
 
     public void markDone() {
